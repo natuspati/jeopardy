7d6669cae2ce806a228d8f261d1f2110b4b4d2fc
Nurlat Bekdullayev <nurlat.bekdullayev@syngenta.com>
2025-03-07 17:55:40 +0500

    Add uvicorn run

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from settings import settings
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="application:app",
+        host=settings.host,
+        port=settings.port,
+        reload=settings.reload,
+    )
